85336b9cbe0f5be5c4829fd9211cca533241fbd2
Om <88827091+mobileappdev47@users.noreply.github.com>
2022-06-23 14:33:59 +0530

    changes

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -14,39 +14,23 @@ class Strings {
   static const forAdvertise = " for advertiser's account";
   static const termsServices =
       'By continuing you agree Rainbow’s Terms of Services & Privacy Policy.';
-
   static const welcomeBack = 'Welcome Back!';
-
   static const userName = 'User name';
-
   static const passWord = 'Password';
-
   static const forgotPassword = 'Forgot password?';
-
   static const login = 'Login';
-
   static const log_in = 'Log in';
-
   static const loginAsAdvertiser = ' as advertiser';
-
   static const dontHaveAccount = ' Don’t have an account';
-
   static const getStarted = 'Get Started';
-
   static const notVerified = 'Your Account \nis not yet verified';
-
   static const completeYourProfile =
       'Complete your profile to unlock \nmore rainbow feature';
-
   static const important = 'Important';
-
   static const smoothProcess =
       'To ensure a smooth process, please read the following reminders:';
-
   static const prepare = '-Prepare your valid ID';
-
   static const success = 'Success!';
-
   static const allow =
       'Please allow up to 1 business day for our team to review your ID submission';
 
@@ -64,6 +48,6 @@ class Strings {
   static const birthDate = 'Birth Date';
   static const noOfKids = 'No of Kids';
   static const alreadyHaveAnAccount = 'Already have an account? ';
-  static const signIn = 'Sign in';
-  static const privacyPolicyText = 'By continuing you agree Rainbow’s Terms of Services & Privacy Policy.';
+  static const privacyPolicyText =
+      'By continuing you agree Rainbow’s Terms of Services & Privacy Policy.';
 }
