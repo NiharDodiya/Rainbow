8db9da97c5293f004173dc77f3f89ae3b5295df7
Om <88827091+mobileappdev47@users.noreply.github.com>
2022-07-04 18:31:54 +0530

    responsive UI of register_screen

--- lib/screens/auth/register/register_screen.dart
+++ lib/screens/auth/register/register_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rainbow/common/Widget/app_dropdown.dart';
+import 'package:rainbow/common/Widget/buttons.dart';
 import 'package:rainbow/common/Widget/loaders.dart';
 import 'package:rainbow/common/Widget/text_styles.dart';
 import 'package:rainbow/screens/auth/register/register_controller.dart';
@@ -23,76 +24,74 @@ class RegisterScreen extends StatelessWidget {
           {
             return const SmallLoader();
           }
-        return SafeArea(
-          child: SingleChildScrollView(
-              padding: EdgeInsets.all(Get.width * 0.02667),
-              child:Container(
-                padding: EdgeInsets.symmetric(horizontal: Get.width * 0.0733),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(36),
-                  color: ColorRes.color_4F359B,
-                ),
-                child: GetBuilder<RegisterController>(
-                  id: 'register_screen',
-                  builder: (controller) {
-                    return Stack(
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            /// top bar area
-                            SizedBox(height: Get.height * 0.0616),
-                            Image.asset(
-                              AssetRes.rainBowLogo,
-                              height: Get.height * 0.05541,
-                            ),
-                            SizedBox(height: Get.height * 0.0308),
-                            Text(Strings.register,
-                                style: textStyleFont30WhiteExtraBold),
-                            SizedBox(height: Get.height * 0.02094),
+        return SingleChildScrollView(
+            padding: EdgeInsets.all(Get.width * 0.02667),
+            child:Container(
+              padding: EdgeInsets.symmetric(horizontal: Get.width * 0.0733),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(36),
+                color: ColorRes.color_4F359B,
+              ),
+              child: GetBuilder<RegisterController>(
+                id: 'register_screen',
+                builder: (controller) {
+                  return Stack(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          /// top bar area
+                          SizedBox(height: Get.height * 0.0616),
+                          Image.asset(
+                            AssetRes.rainBowLogo,
+                            height: Get.height * 0.05541,
+                          ),
+                          SizedBox(height: Get.height * 0.0308),
+                          Text(Strings.register,
+                              style: textStyleFont30WhiteExtraBold),
+                          SizedBox(height: Get.height * 0.02094),
 
-                            /// form
-                            RegisterForm(),
+                          /// form
+                          RegisterForm(),
 
-                            /// register button
-                            registerButtons(controller),
-                          ],
-                        ),
-                        GetBuilder<RegisterController>(id: 'register_screen' ,builder: (controller) {
-                          return     !controller.martialStatusDropdown
-                              ? const SizedBox()
-                              : Positioned(
-                            top: Get.height * 1.213,width: Get.width * 0.8,
-                            child: AppDropdown(
-                              paramList: controller.martialStatusList,
-                              onTap: controller.onStatusChange,
-                            ),
-                          );
-                        },),
-                        !controller.ethnicityDropdown
+                          /// register button
+                          registerButtons(controller),
+                        ],
+                      ),
+                      GetBuilder<RegisterController>(id: 'register_screen' ,builder: (controller) {
+                        return     !controller.martialStatusDropdown
                             ? const SizedBox()
                             : Positioned(
-                          top: Get.height * 1.34,width: Get.width * 0.8,
-                          child: AppDropDownEthnicity(
-                            paramList: controller.ethnicityList,
-                            onTap: controller.onEthnicityChange,
+                          top: Get.height * 1.213,width: Get.width * 0.8,
+                          child: AppDropdown(
+                            paramList: controller.martialStatusList,
+                            onTap: controller.onStatusChange,
                           ),
+                        );
+                      },),
+                      !controller.ethnicityDropdown
+                          ? const SizedBox()
+                          : Positioned(
+                        top: Get.height * 1.34,width: Get.width * 0.8,
+                        child: AppDropDownEthnicity(
+                          paramList: controller.ethnicityList,
+                          onTap: controller.onEthnicityChange,
                         ),
-                        !controller.kidsDropdown
-                            ? const SizedBox()
-                            : Positioned(
-                          top: Get.height * 1.6,width: Get.width * 0.8,
-                          child: AppDropDownNoOfKids(
-                            paramList: controller.noOfKids,
-                            onTap: controller.onKidsChange,
-                          ),
+                      ),
+                      !controller.kidsDropdown
+                          ? const SizedBox()
+                          : Positioned(
+                        top: Get.height * 1.6,width: Get.width * 0.8,
+                        child: AppDropDownNoOfKids(
+                          paramList: controller.noOfKids,
+                          onTap: controller.onKidsChange,
                         ),
-                      ],
-                    );
-                  },
-                ),
-              )
-          ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            )
         );
       })
     );
@@ -103,26 +102,12 @@ class RegisterScreen extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        SizedBox(height: Get.height * 0.02),
-        GestureDetector(
-          onTap: () {
-            controller.onRegisterTap();
-          },
-          child: Container(
-            height: Get.height * 0.076,
-            width: Get.width * 0.85,
-            decoration: BoxDecoration(
-                color: ColorRes.color_E7D01F,
-                borderRadius: BorderRadius.circular(15)),
-            child: Center(
-              child: Text(
-                Strings.register,
-                style: textStyleFont16Black,
-              ),
-            ),
-          ),
+        SizedBox(height: Get.height * 0.015),
+        SubmitButton(
+          text: Strings.register,
+          onTap: controller.onRegisterTap,
         ),
-        SizedBox(height: Get.height * 0.055),
+        SizedBox(height: Get.height * 0.04680),
         Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -142,7 +127,7 @@ class RegisterScreen extends StatelessWidget {
             ),
           ],
         ),
-        SizedBox(height: Get.height * 0.06),
+        SizedBox(height: Get.height * 0.04557),
         SizedBox(
           height: Get.height * 0.036,
           width: Get.width * 0.69,
