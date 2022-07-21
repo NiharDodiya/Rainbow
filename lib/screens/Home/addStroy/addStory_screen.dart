812a742f66633dab358986340174a66012fabe7f
Om <88827091+mobileappdev47@users.noreply.github.com>
2022-07-21 18:58:49 +0530

    changes

--- lib/screens/Home/addStroy/addStory_screen.dart
+++ lib/screens/Home/addStroy/addStory_screen.dart
@@ -3,9 +3,12 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:photo_manager/photo_manager.dart';
+import 'package:rainbow/common/Widget/loaders.dart';
 import 'package:rainbow/screens/Home/addStroy/addStory_controller.dart';
 import 'package:rainbow/screens/Home/addStroy/widgets/addStoryAppbar.dart';
 import 'package:rainbow/screens/Home/addStroy/widgets/addStoryCamera.dart';
+import 'package:rainbow/utils/asset_res.dart';
+import 'package:rainbow/utils/color_res.dart';
 
 class AddStoryScreen extends StatefulWidget {
   AddStoryScreen({Key? key}) : super(key: key);
@@ -17,100 +20,117 @@ class AddStoryScreen extends StatefulWidget {
 class _AddStoryScreenState extends State<AddStoryScreen> {
   AddStoryController controller = Get.put(AddStoryController());
 
-  ScrollController? scrollController;
+  ScrollController scrollController = ScrollController();
   List<Widget> _mediaList = [];
   int currentPage = 0;
   int? lastPage;
+  int totalVideos = 0;
+  List<AssetEntity> videoList = [];
 
   @override
   void initState() {
     super.initState();
-    scrollController = ScrollController()
-      ..addListener(() {
-        _handleScrollEvent();
-      });
-    _fetchNewMedia();
-  }
-
-  _handleScrollEvent() {
-    if (scrollController!.position.pixels /
-            scrollController!.position.maxScrollExtent >
-        0.33) {
-      if (currentPage != lastPage) {
-        _fetchNewMedia();
+    fetchNewVideos();
+    scrollController.addListener(() async {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        double position = scrollController.position.pixels;
+        if (videoList.length < totalVideos) {
+          await fetchNewVideos().then((value) {
+            Future.delayed(const Duration(milliseconds: 100), () {
+              scrollController.jumpTo(position + 200);
+            });
+          });
+        }
       }
-    }
+    });
   }
 
-  _fetchNewMedia() async {
+  Future<void> fetchNewVideos() async {
     lastPage = currentPage;
-    PermissionState result = await PhotoManager.requestPermissionExtend();
-    if (result == PermissionState.authorized) {
-      // success
-//load the album list
-      List<AssetPathEntity> albums =
-          await PhotoManager.getAssetPathList(onlyAll: true);
-      List<AssetEntity> media =
-          await albums[0].getAssetListPaged(page: 1, size: 100);
-      print(media);
-      List<Widget> temp = [];
-      for (var asset in media) {
-        temp.add(
-          FutureBuilder<Uint8List?>(
-            future: asset.thumbnailData,
-            builder: (BuildContext context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                return InkWell(
-                  onTap: () => controller.onImageTap(asset.file),
-                  child: Stack(
-                    children: <Widget>[
-                      Positioned.fill(
-                        child: Image.memory(
-                          snapshot.data!,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }
-              return const SizedBox();
-            },
-          ),
+    controller.loader.value = true;
+    List<AssetPathEntity> albums = await PhotoManager.getAssetPathList(
+        onlyAll: true, type: RequestType.image);
+
+    totalVideos = albums[0].assetCount;
+    List<AssetEntity> media =
+        await albums[0].getAssetListPaged(page: currentPage, size: 60);
+
+    media.forEach((element) {
+      if (element.size > Offset.zero) {
+        videoList.add(
+          AssetEntity(
+              id: element.id,
+              typeInt: element.typeInt,
+              width: element.width,
+              height: element.height),
         );
       }
-      setState(() {
-        _mediaList.addAll(temp);
-        currentPage++;
-      });
-    } else {
-      // fail
-    }
+    });
+    currentPage++;
+    controller.loader.value = false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            addStoryAppBar(controller),
-            SizedBox(
-              height: Get.height * 0.04,
+      body: Obx(() {
+        if (controller.loader.isTrue) {
+          return const SmallLoader();
+        } else {
+          return SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                addStoryAppBar(controller),
+                SizedBox(
+                  height: Get.height * 0.04,
+                ),
+                Expanded(
+                  child: GridView.builder(
+                    controller: scrollController,
+                    itemCount: videoList.length +1,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      childAspectRatio: 1.1 / 2,
+                      mainAxisSpacing: 2,
+                      crossAxisSpacing: 2,
+                    ),
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return addStoryCamera(controller);
+                      }
+                      return FutureBuilder<Uint8List?>(
+                        future: videoList[index-1].thumbnailDataWithSize(
+                            const ThumbnailSize(200, 200)),
+                        builder: (context, snapshot) {
+                          return snapshot.hasData
+                              ? InkWell(
+                                  onTap: () => controller
+                                      .onImageTap(videoList[index-1].file),
+                                  child: Image.memory(
+                                    //model.videoAvatar[index],
+                                    snapshot.data!,
+                                    fit: BoxFit.cover,
+                                  ),
+                                )
+                              : Image.asset(
+                                  AssetRes.home,
+                                  color: ColorRes.red.withOpacity(0.5),
+                                );
+                        },
+                      );
+                    },
+                  ),
+                )
+                // gallaryImage()
+              ],
             ),
-
-            // addStoryCamera(controller),
-            /*  InkWell(onTap: () {
-            Get.to(MyApp());
-          },
-              child: const Text("submit",style:TextStyle(color: Colors.black),)),*/
-            gallaryImage()
-          ],
-        ),
-      ),
+          );
+        }
+      }),
     );
   }
 
--- lib/screens/Home/addStroy/widgets/addStoryCamera.dart
+++ lib/screens/Home/addStroy/widgets/addStoryCamera.dart
@@ -7,37 +7,32 @@ import 'package:rainbow/utils/color_res.dart';
 import 'package:rainbow/utils/strings.dart';
 
 Widget addStoryCamera(AddStoryController controller) {
-  return Column(
-    children: [
-      Container(
-        height: Get.height * 0.31,
-        width: Get.width * 0.328,
-        color: ColorRes.color_F4F4F4,
-        child: InkWell(
-          onTap: () {
-            controller.camera();
-          },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                height: 19.5,
-                width: 19.5,
-                decoration: const BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage(AssetRes.shapeCamera),
-                        fit: BoxFit.cover)),
-              ),
-              Text(
-                Strings.camera,
-                style: beVietnamSemiBoldTextStyle(
-                    fontSize: 12, color: ColorRes.color_252525),
-              )
-            ],
+  return Container(
+    width: Get.width * 0.328,
+    color: ColorRes.color_F4F4F4,
+    child: InkWell(
+      onTap: () {
+        controller.camera();
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 19.5,
+            width: 19.5,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(AssetRes.shapeCamera),
+                    fit: BoxFit.cover)),
           ),
-        ),
-      )
-    ],
+          Text(
+            Strings.camera,
+            style: beVietnamSemiBoldTextStyle(
+                fontSize: 12, color: ColorRes.color_252525),
+          )
+        ],
+      ),
+    ),
   );
 }
