b7c3892e3eceefd822df681ba58bf037c0e3dbc6
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2022-11-01 17:32:51 +0530

    amount

--- lib/screens/advertisement/ad_home/widget/advertisement_list.dart
+++ lib/screens/advertisement/ad_home/widget/advertisement_list.dart
@@ -242,7 +242,8 @@ Widget advertisementList() {
                                         ),
                                         const Spacer(),
                                         Text(
-                                          "\$200",
+                                          "£${controller.myAdvertiserModel
+                                              .data?[index].amount ??""}",
                                           style: gilroySemiBoldTextStyle(
                                               fontSize: 14,
                                               color: ColorRes.black),
