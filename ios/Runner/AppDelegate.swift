fd26bf8ebba3c5380600420a192e3eb45d07769e
mobileappdev47 <88827091+mobileappdev47@users.noreply.github.com>
2022-11-30 19:16:25 +0530

    changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,5 @@
 import UIKit
 import Flutter
-import FirebaseCore
-import FBSDKCoreKit
-import GoogleMaps
-import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,53 +7,7 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-      
     GeneratedPluginRegistrant.register(with: self)
-
-    GMSServices.provideAPIKey("AIzaSyDlNJdlotYUUVPg8iAVMmBa16xIsOjhzUY")
-      if FirebaseApp.app() == nil {
-          FirebaseApp.configure()
-      }
-      
-      if #available(iOS 10.0, *) {
-            // For iOS 10 display notification (sent via APNS)
-            UNUserNotificationCenter.current().delegate = self
-
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-              options: authOptions,
-              completionHandler: {_, _ in })
-          } else {
-            let settings: UIUserNotificationSettings =
-            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-          }
-
-          application.registerForRemoteNotifications()
-
-          Messaging.messaging().token { token, error in
-            if let error = error {
-              print("Error fetching FCM registration token: \(error)")
-            } else if let token = token {
-              print("FCM registration token:==> \(token)")
-
-      //        self.fcmRegTokenMessage.text  = "Remote FCM registration token: \(token)"
-            }
-          }
-
-      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-    
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-            let handledFB = ApplicationDelegate.shared.application(app, open: url, options: options)
-            return handledFB
-        }
-    
-    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-         print("Registered for Apple Remote Notifications")
-         Messaging.messaging().apnsToken = deviceToken
-         Messaging.messaging().setAPNSToken(deviceToken, type: .unknown)
-
-     }
 }
